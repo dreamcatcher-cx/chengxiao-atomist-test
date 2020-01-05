8c3f203d2416a9f981606ae5d87168df61f682c5
dreamcatcher-cx <578893745@qq.com>
2020-01-05 10:40:09 +0800

    Initial commit from Atomist

---
+++ src/main/java/chengxiao/ChengxiaoAtomistTestApplication.java
@@ -0,0 +1,12 @@
+package chengxiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChengxiaoAtomistTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChengxiaoAtomistTestApplication.class, args);
+	}
+}
